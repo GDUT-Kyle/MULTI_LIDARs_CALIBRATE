48e1965792cf204b263ef4c85985fba2f2a1eed9
GDUT-Kyle <1243588305@qq.com>
2022-05-18 16:55:09 +0800

    fix bug

--- multi_lidar_calibration/src/calibration_node.cpp
+++ multi_lidar_calibration/src/calibration_node.cpp
@@ -2,6 +2,79 @@
 #include "multi_lidar_calibration/loadBag.h"
 #include "multi_lidar_calibration/calibration.h"
 
+void calibrateLivoxAndMap(bag_loader& LivoxLoader, const loaderTrajAndMap& MapTrajsLoader, 
+                const boost::shared_ptr<pcl::visualization::PCLVisualizer> visual,
+                const pcl::visualization::PointCloudColorHandlerCustom<Point>& matchColor, 
+                Eigen::Matrix4d& initExtMat)
+{
+    // 遍历所有livox点云，逐个与map进行ndt匹配
+    size_t leftOdomIndex = 0;
+    size_t rightOdomIndex = 0;
+    Eigen::Matrix<double, 6, 1> alignExtMatSum = Eigen::Matrix<double, 6, 1>::Zero();
+    int validCounter = 0;
+    auto& Trajs = MapTrajsLoader.traj;
+    auto& Livoxs = LivoxLoader.LivoxPcls;
+    for(int i=0; i<LivoxLoader.LivoxPcls.size()&&ros::ok(); i++)
+    {
+        // 在导入包的时候就已经确保所有livox消息的时间被包含在odometry的时间跨度内，所以不用做溢出判断
+        while(Trajs[rightOdomIndex].timestamp<Livoxs[i].timestamp)
+            rightOdomIndex++;
+        leftOdomIndex = rightOdomIndex-1;
+
+        double s = (Livoxs[i].timestamp-Trajs[leftOdomIndex].timestamp)/(Trajs[rightOdomIndex].timestamp-Trajs[leftOdomIndex].timestamp);
+        
+        Transform livoxTrans;
+        livoxTrans.timestamp = Livoxs[i].timestamp;
+        livoxTrans.rotation = Trajs[rightOdomIndex].rotation.slerp(s, Trajs[leftOdomIndex].rotation);
+        livoxTrans.transition = (1.0-s)*Trajs[leftOdomIndex].transition + s*Trajs[rightOdomIndex].transition;
+
+        Eigen::Matrix<double, 4, 4> baseMapMat = Eigen::Matrix<double, 4, 4>::Identity();
+        baseMapMat.block(0, 0, 3, 3) = livoxTrans.rotation.toRotationMatrix();
+        baseMapMat.block(0, 3, 3, 1) = livoxTrans.transition;
+        Eigen::Matrix<double, 4, 4> initGuass = baseMapMat*initExtMat; // 配置初始估计
+
+        PointCloud::Ptr outputCloudPtr(new PointCloud);
+        double fitnessScore = -1.0;
+
+        #ifdef USE_GICP
+        _gicp.setInputSource(Livoxs[i].pointcloud.makeShared());
+        _gicp.align(*outputCloudPtr, initGuass.cast<float>());
+        Eigen::Matrix4f alignTrans = _gicp.getFinalTransformation();
+        fitnessScore = _gicp.getFitnessScore();
+        initExtMat = (baseMapMat.cast<float>().inverse() *alignTrans).cast<double>();
+        #else
+        _ndt.setInputSource(Livoxs[i].pointcloud.makeShared());
+        _ndt.align(*outputCloudPtr, initGuass.cast<float>());
+        Eigen::Matrix4f alignTrans = _ndt.getFinalTransformation();
+        // update the extrinsic Matrix (based rslidar)
+        fitnessScore = _ndt.getFitnessScore();
+        initExtMat = (baseMapMat.cast<float>().inverse() *alignTrans).cast<double>();
+        #endif
+
+        Eigen::Matrix3d rotaMat = initExtMat.block(0, 0, 3, 3);
+        Eigen::AngleAxisd angle = Eigen::AngleAxisd(rotaMat);
+
+        if(fitnessScore < 0.3)
+        {
+            validCounter++;
+            alignExtMatSum.block(0, 0, 3, 1) += angle.angle()*(angle.axis());
+            alignExtMatSum.block(3, 0, 3, 1) += initExtMat.block(0, 3, 3, 1);
+            cout<<alignExtMatSum.transpose()/validCounter<<endl<<endl;
+        }
+
+        visual->updatePointCloud<Point>(outputCloudPtr, matchColor, "match cloud");
+        visual->spinOnce();
+    }
+
+    alignExtMatSum = alignExtMatSum/validCounter;
+    Eigen::Vector3d angleVec = alignExtMatSum.block(0, 0, 3, 1);
+    Eigen::AngleAxisd newExtRotation(angleVec.norm(), angleVec.normalized());
+    Eigen::Vector3d newExtTranslate = alignExtMatSum.block(3, 0, 3, 1);
+    initExtMat.block(0, 0, 3, 3) = newExtRotation.toRotationMatrix();
+    initExtMat.block(0, 3, 3, 1) = newExtTranslate;
+    cout<<"Final result: \n"<<initExtMat<<endl;
+}
+
 int main(int argc, char **argv)
 {
     // 实例化ros节点
@@ -64,75 +137,10 @@ int main(int argc, char **argv)
     viewer_final->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "target cloud");
     viewer_final->addPointCloud<Point>(bag_loader_.LivoxPcls[0].pointcloud.makeShared(), match_color, "match cloud"); //display the match cloud
 
-
-    // 遍历所有livox点云，逐个与map进行ndt匹配
-    size_t leftOdomIndex = 0;
-    size_t rightOdomIndex = 0;
-    Eigen::Matrix<double, 6, 1> alignExtMatSum = Eigen::Matrix<double, 6, 1>::Zero();
-    int validCounter = 0;
-    auto& Trajs = _map_and_traj_loader.traj;
-    auto& Livoxs = bag_loader_.LivoxPcls;
-    for(int i=0; i<bag_loader_.LivoxPcls.size()&&ros::ok(); i++)
-    {
-        // 在导入包的时候就已经确保所有livox消息的时间被包含在odometry的时间跨度内，所以不用做溢出判断
-        while(Trajs[rightOdomIndex].timestamp<Livoxs[i].timestamp)
-            rightOdomIndex++;
-        leftOdomIndex = rightOdomIndex-1;
-
-        double s = (Livoxs[i].timestamp-Trajs[leftOdomIndex].timestamp)/(Trajs[rightOdomIndex].timestamp-Trajs[leftOdomIndex].timestamp);
-        
-        Transform livoxTrans;
-        livoxTrans.timestamp = Livoxs[i].timestamp;
-        livoxTrans.rotation = Trajs[rightOdomIndex].rotation.slerp(s, Trajs[leftOdomIndex].rotation);
-        livoxTrans.transition = (1.0-s)*Trajs[leftOdomIndex].transition + s*Trajs[rightOdomIndex].transition;
-
-        Eigen::Matrix<double, 4, 4> baseMapMat = Eigen::Matrix<double, 4, 4>::Identity();
-        baseMapMat.block(0, 0, 3, 3) = livoxTrans.rotation.toRotationMatrix();
-        baseMapMat.block(0, 3, 3, 1) = livoxTrans.transition;
-        Eigen::Matrix<double, 4, 4> initGuass = baseMapMat*ExtMatrix; // 配置初始估计
-
-        PointCloud::Ptr outputCloudPtr(new PointCloud);
-        double fitnessScore = -1.0;
-
-        #ifdef USE_GICP
-        _gicp.setInputSource(Livoxs[i].pointcloud.makeShared());
-        _gicp.align(*outputCloudPtr, initGuass.cast<float>());
-        Eigen::Matrix4f alignTrans = _gicp.getFinalTransformation();
-        fitnessScore = _gicp.getFitnessScore();
-        ExtMatrix = (baseMapMat.cast<float>().inverse() *alignTrans).cast<double>();
-        #else
-        _ndt.setInputSource(Livoxs[i].pointcloud.makeShared());
-        _ndt.align(*outputCloudPtr, initGuass.cast<float>());
-        Eigen::Matrix4f alignTrans = _ndt.getFinalTransformation();
-        // update the extrinsic Matrix (based rslidar)
-        fitnessScore = _ndt.getFitnessScore();
-        ExtMatrix = (baseMapMat.cast<float>().inverse() *alignTrans).cast<double>();
-        #endif
-
-        Eigen::Matrix3d rotaMat = ExtMatrix.block(0, 0, 3, 3);
-        Eigen::AngleAxisd angle = Eigen::AngleAxisd(rotaMat);
-
-        // cout<<angle.angle()*(angle.axis().transpose())<<endl<<endl;
-        if(fitnessScore < 0.3)
-        {
-            validCounter++;
-            alignExtMatSum.block(0, 0, 3, 1) += angle.angle()*(angle.axis());
-            alignExtMatSum.block(3, 0, 3, 1) += ExtMatrix.block(0, 3, 3, 1);
-            cout<<alignExtMatSum.transpose()/validCounter<<endl<<endl;
-        }
-
-        viewer_final->updatePointCloud<Point>(outputCloudPtr, match_color, "match cloud");
-        viewer_final->spinOnce();
-    }
-
-    alignExtMatSum = alignExtMatSum/validCounter;
-    Eigen::Vector3d angleVec = alignExtMatSum.block(0, 0, 3, 1);
-    Eigen::AngleAxisd newExtRotation(angleVec.norm(), angleVec.normalized());
-    Eigen::Vector3d newExtTranslate = alignExtMatSum.block(3, 0, 3, 1);
-    ExtMatrix.block(0, 0, 3, 3) = newExtRotation.toRotationMatrix();
-    ExtMatrix.block(0, 3, 3, 1) = newExtTranslate;
-    cout<<"Final result: \n"<<ExtMatrix<<endl;
-
+    calibrateLivoxAndMap(bag_loader_, _map_and_traj_loader, viewer_final, match_color, ExtMatrix);
+    // Other Livox LiDAR
+    // calibrateLivoxAndMap(bag_loader_, _map_and_traj_loader, viewer_final, match_color, ExtMatrix);
+    
     viewer_final->close();
 
     return 0;
